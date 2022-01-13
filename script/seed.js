2bbaf4ff0860e9123c8fa93c62c61952902e5502
Gal Gir <ggir@mail.yu.edu>
2022-01-13 15:35:28 -0500

    Book db

--- script/seed.js
+++ script/seed.js
@@ -18,7 +18,7 @@ async function seed() {
 
   //creating Books
   let stream = fs.createReadStream(
-    "/Users/chinahoffman/src/Fullstack/2111-grace-shopper/script/book_data_selection.csv"
+    "./script/book_data_selection.csv"
   );
   let csvData = [];
   let csvStream = fastcsv
