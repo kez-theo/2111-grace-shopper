a1813a2884481641c118bebff797d0700720ab2e
Gal Gir <ggir@mail.yu.edu>
2022-01-18 14:51:24 -0500

    save

--- public/bundle.js
+++ public/bundle.js
@@ -2819,7 +2819,8 @@ class SingleBook extends react__WEBPACK_IMPORTED_MODULE_0__.Component {
     }, "Add to Cart"));
   }
 
-}
+} //book has a bought property that once it is bought it is true. (keep in mind for button)
+
 
 const mapStateToProps = state => ({
   book: state.singleBookReducer
