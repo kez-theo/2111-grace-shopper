cfd6df5eb1f71e88b73d3f51f50c550825a7f3f1
keranie theodosiou <keranie.theodosiou@gmail.com>
2022-01-19 15:40:37 -0500

    updated FormStockItem component

--- client/components/FormStockItem.js
+++ client/components/FormStockItem.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react"
+import { connect } from "react-redux"
 import { updateStock } from "../store/stockItem";
 
 
@@ -62,68 +62,12 @@ class FormStockItem extends React.Component {
     );
   }
 }
-// const FormStockItem = () => {
-//     const dispatch = useDispatch();
-//     const { book } = useSelector((state) => {
-//         return {
-//             book: state.stockItemReducer
-//         }
-//     })
 
-//     useEffect(() => {
-//         dispatch(fetchStockItem(book.id))
-//     }, [])
+const mapStateToProps = ({ book }) => ({
+  book
+})
 
-//     return (
-//         <div>
-//           <h1>Edit a Book</h1>
-//           <form>
-//             <label htmlFor="title">Edit Book Title</label>
-//             <input name="title" value={title} />
-
-//             <label htmlFor="series">Edit Series</label>
-//             <input name="series" value={series} />
-
-//             <label htmlFor="author">Edit Author</label>
-//             <input name="author" value={author} />
-
-//             <label htmlFor="description">Edit Description</label>
-//             <input name="description" value={description} />
-
-//             <label htmlFor="language">Edit Langauge</label>
-//             <input name="language" value={language} />
-
-//             <label htmlFor="isbn">Edit Isbn</label>
-//             <input name="isbn" value={isbn} />
-
-//             <label htmlFor="genres">Edit Genres</label>
-//             <input name="genres" value={genres} />
-
-//             <label htmlFor="bookformat">Edit Book Format</label>
-//             <input name="bookformat" value={bookformat} />
-
-//             <label htmlFor="pages">Edit Page Numbers</label>
-//             <input name="pages" value={pages} />
-
-//             <label htmlFor="publisher">Edit Publisher</label>
-//             <input name="publisher" value={publisher} />
-
-//             <label htmlFor="coverimg">Edit Cover Image</label>
-//             <input name="coverimg" value={coverimg} />
-
-//             <label htmlFor="price">Edit Price</label>
-//             <input name="price" value={price} />
-//           </form>
-
-//           {/* <h1>{book.title}</h1>
-//           <p>{book.author}</p>
-//           <img src={book.coverimg} style={{ width: "220px", height: "350px" }} />
-//           <p> Description: {book.description}</p>
-//           <h4>${book.price ? book.price / 100 : 5}</h4>
-//           <button>
-//             Update Item
-//           </button> */}
-//         </div>
-//     );
-// }
-export default FormStockItem;
+const mapDispatch = (dispatch, { history}) => ({
+  updateStock: (stockItem) => dispatch(updateStock(stockItem, history))
+})
+export default connect(mapStateToProps, mapDispatch)(FormStockItem);
--- client/components/StockItem.js
+++ client/components/StockItem.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link, useParams } from "react-router-dom"
 import { fetchStockItem } from "../store/stockItem";
 
 const StockItem = () => {
     const dispatch = useDispatch();
+    const match = useParams();
     const { book } = useSelector((state) => {
         return {
             book: state.stockItemReducer
@@ -11,7 +13,8 @@ const StockItem = () => {
     })
 
     useEffect(() => {
-        dispatch(fetchStockItem(book.id))
+        console.log(book)
+        dispatch(fetchStockItem(match.stockId))
     }, [])
 
     return (
@@ -20,11 +23,14 @@ const StockItem = () => {
           <p>{book.author}</p>
           <img src={book.coverimg} style={{ width: "220px", height: "350px" }} />
           <p> Description: {book.description}</p>
-          <h4>${book.price ? book.price / 100 : 5}</h4>
-          <button>
-            Update Item
-          </button>
+          <h4>${book.price ? (book.price/100).toFixed(2) : 5.00}</h4>
+          <Link to={`/stock/${book.id}/edit-stock`}>
+            <button>
+                Update Item
+            </button>
+          </Link>
         </div>
     );
 }
+
 export default StockItem;
